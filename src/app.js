ca66430561fc73497cb0ea1d9b2768e255802d9e
AjayKumarNallanagula <kumarnajay9@gmail.com>
2019-04-18 00:48:59 +0530

    Node simple server running on port 4242

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+res.end('hello world');
+});
+
+server.listen(4242,()=>{
+    console.log('Server is listening on  port 4242, url: http://localhost:4242')
+})
